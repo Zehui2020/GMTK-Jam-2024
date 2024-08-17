c4e09c6529ae53b0d39f67d0645577c672be341a
Rageking8 <tomleetyt@gmail.com>
2024-08-17 15:44:11 +0800

    Implement crude camera confiner

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
+    private Vector2 _xBounds;
+
+    [SerializeField]
+    private Vector2 _yBounds;
+
+    [SerializeField]
     private float _speed = 10f;
 
     private void Update()
@@ -10,5 +16,9 @@ public class CameraController : MonoBehaviour
         transform.Translate(new(Input.GetAxis("Horizontal") * _speed *
             Time.deltaTime, Input.GetAxis("Vertical") * _speed *
             Time.deltaTime, 0f));
+
+        transform.position = new(Mathf.Clamp(transform.position.x,
+            _xBounds.x, _xBounds.y), Mathf.Clamp(transform.position.y,
+            _yBounds.x, _yBounds.y), transform.position.z);
     }
 }
