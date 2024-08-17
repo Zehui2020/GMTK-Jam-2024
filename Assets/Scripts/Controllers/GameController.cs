60bdb925581dea923751592a39c9ccbc1b99e1b1
Pokerx95 <123877073+Pokerx95@users.noreply.github.com>
2024-08-17 12:22:57 +0800

    Setup

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //Variables
+
+
+
+    //Singleton
+    private GameController instance;
+    public static GameController Instance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
