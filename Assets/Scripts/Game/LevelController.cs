92440a3d62701599c28981afdfafa2357b4b6fcd
Christian <97773844+esuriru@users.noreply.github.com>
2024-08-17 15:07:16 +0800

    Add and init gameplay variables in `LevelController`

--- Assets/Scripts/Game/LevelController.cs
+++ Assets/Scripts/Game/LevelController.cs
@@ -1,7 +1,27 @@
 using UnityEngine;
 
+using static DebugUtility;
+
 public class LevelController : MonoBehaviour
 {
     // TODO (Chris): To be set from the selection screen, or a manager
     public Level CurrentLevel { get; set; }
+
+    private int _waveIndex; 
+    private float _timer;
+
+    public void Init()
+    {
+        AssertNotNull(CurrentLevel, "Current level is null");
+        Assert(CurrentLevel.Waves.IsEmpty(), "Current level waves are empty");
+
+        // Init gameplay variables
+        _waveIndex = 0;
+        _timer = 0.0f;
+    }
+    
+    public void Play()
+    {
+
+    }
 }
