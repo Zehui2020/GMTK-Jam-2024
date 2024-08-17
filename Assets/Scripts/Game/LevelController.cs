7824be665b94552480453dc7ccf31b174d1e29b6
Christian <97773844+esuriru@users.noreply.github.com>
2024-08-17 14:16:45 +0800

    Create base `LevelController` level manager script

---
+++ Assets/Scripts/Game/LevelController.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    // TODO (Chris): To be set from the selection screen, or a manager
+    public Level CurrentLevel { get; set; }
+}
