a2bee06ac388aa6a17f641d646ae6962f1d9f8d9
Christian <97773844+esuriru@users.noreply.github.com>
2024-08-17 14:42:17 +0800

    Add list of `Wave` inside of `Level`

--- Assets/Scripts/Game/Level.cs
+++ Assets/Scripts/Game/Level.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Scriptable Object/Level", fileName = "New Level")]
 public class Level : ScriptableObject
 {
-    // TODO (Chris): Store waves
+    [field: Header("Level Dependencies")]
+
+    [field: SerializeField]
+    public List<Wave> Waves { get; private set; }
 }
