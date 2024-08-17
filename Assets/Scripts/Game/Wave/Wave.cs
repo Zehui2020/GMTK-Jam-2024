369811939fdc0a8f6bda7618bffbac0958e0c91c
Christian <97773844+esuriru@users.noreply.github.com>
2024-08-17 14:38:41 +0800

    Create list of `WaveNode` inside `Wave`

--- Assets/Scripts/Game/Wave/Wave.cs
+++ Assets/Scripts/Game/Wave/Wave.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Scriptable Object/Wave", fileName = "New Wave")]
 public class Wave : ScriptableObject 
 {
+    [field: Header("Wave Settings")]
+
+    [field: SerializeField]
+    public List<WaveNode> Nodes { get; private set; } 
 
 }
