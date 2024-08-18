5f31a62fe57e8f071d8a4f0ee2b8ad047e2c539e
ChongChiSengVincent <TheEmeraldsPlayer@gmail.com>
2024-08-18 14:31:41 +0800

    Add `TutorialData` Script
    storing tutorial data

--- Assets/Scripts/Tutorial/TutorialController.cs
+++ Assets/Scripts/Tutorial/TutorialController.cs
@@ -1,9 +1,34 @@
-using UnityEngine;
+using System.Collections.Generic;
 
+using TMPro;
+using UnityEngine;
 public class TutorialController : MonoBehaviour
 {
+    [SerializeField] private List<TutorialData> _dataList;
+
+    [SerializeField] private GameObject _textboxContainer;
+
+    [SerializeField] private TMP_Text _instructionsText;
+
+    private int _currentTutorialNumber;
+    private int _currentTutorialSet;
     public void CheckNewWave(int newWave)
     {
         Debug.Log("wave " + newWave);
+    }
+
+    private void Start()
+    {
+        _currentTutorialNumber = 0;
+        _currentTutorialSet = 0;
+    }
+    private void Update()
+    {
+        
+    }
+
+    private void ShowNextTutorial()
+    {
+        
     }
 }
---
+++ Assets/Scripts/Tutorial/TutorialData.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+//This data will store each tutorial segment
+[System.Serializable]
+public class TutorialData
+{
+    public List<Vector3> ContainerPosition;
+
+    public List<string> InstructionsText;
+}
