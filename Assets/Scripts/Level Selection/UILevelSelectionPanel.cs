ae0cb418985aacb411ca2f5bc3d8e74af5139152
ChongChiSengVincent <TheEmeraldsPlayer@gmail.com>
2024-08-20 20:14:14 +0800

    level selection locking and unlocking

--- Assets/Scripts/Level Selection/UILevelSelectionPanel.cs
+++ Assets/Scripts/Level Selection/UILevelSelectionPanel.cs
@@ -1,13 +1,44 @@
-using UnityEngine;
+using System.Collections.Generic;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+using UnityEngine;
+using UnityEngine.UI;
+
 
 [RequireComponent(typeof(LoadScene))]
 public class UILevelSelectedPanel : MonoBehaviour
 {
     private LoadScene _loadScene;
+
+    [SerializeField]
+    private List<Image> _tentImages;
+
+    [SerializeField]
+    private List<GameObject> _tentLockGO;
+
+    [SerializeField]
+    private List<Button> _tentButtons;
+
+    [SerializeField]
+    private PlayerDataSO _playerDataSO;
+
+    private void OnEnable()
+    {
+        for (int tentno = 0; tentno < _tentImages.Count; tentno++)
+        {
+            if (_playerDataSO.MaxUnlockedLevel >= tentno)
+            {
+                _tentImages[tentno].color = new(1, 1, 1,1);
+                _tentLockGO[tentno].SetActive(false);
+                _tentButtons[tentno].interactable = true;
+            }
+            else
+            {
+                _tentImages[tentno].color = new(0.5f, 0.5f, 0.5f,1);
+                _tentLockGO[tentno].SetActive(true);
+                _tentButtons[tentno].interactable = false;
+            }
+        }
+    }
 
     public void OnBackButtonPress()
     {
