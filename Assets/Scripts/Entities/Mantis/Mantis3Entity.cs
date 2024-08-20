32c14937d94bc46ecc2cb0e386c75a4c2b0a526b
Pokerx95 <123877073+Pokerx95@users.noreply.github.com>
2024-08-20 14:18:35 +0800

    Mantis Projectile Effect

--- Assets/Scripts/Entities/Mantis/Mantis3Entity.cs
+++ Assets/Scripts/Entities/Mantis/Mantis3Entity.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Mantis3Entity : BaseEntity
 {
     private float attackAbilityTraitCounter = 0;
+    [SerializeField] private GameObject _projectileEffectPrefab;
 
     public override void HandlePassiveTrait()
     {
@@ -45,5 +46,10 @@ public class Mantis3Entity : BaseEntity
             attackAbilityTraitCounter = entityStats.attackTraitCooldown;//15 sec
             DealStatusEffect(EntityStatusEffect.Fear, 3);
         }
+
+        //projectile
+        GameObject newObj = Instantiate(_projectileEffectPrefab);
+        newObj.transform.rotation = transform.rotation;
+        newObj.transform.position = transform.position + (_targetPoint.position - transform.position).normalized * entityStats.detectRange;
     }
 }
